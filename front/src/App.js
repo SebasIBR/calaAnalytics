11e2954b45ed475d3d57c9b9e5c3d386eb6a1063
SebasIBR <94653681+SebasIBR@users.noreply.github.com>
2022-10-08 18:41:59 -0500

    Actualizaciones en el front

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import logo from './logo.JPG'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import {faBoxOpen, faUser, faUsers} from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div>
+      <title>Cala Analitycs</title>
     </div>
-  );
-}
+    <div className="backgroundGray">
+      <header className="header">
+        <div className="header-main-top">
+          <img src={logo} className="logo" alt="logo"/>
+        </div>
+      </header>
+      <div className="admin">
+        Centro de Administracion Cala Analitycs
+      </div>
+      <main className="main">
+        <div className="label">
+          <article className="titulo">Bienvenido</article>
+        </div>
+        <div className="label">
+          <article className="titulo">Bienvenido</article>
+        </div>
+        <div className="label">
+          <article className="titulo">Bienvenido</article>
+        </div>
+        
 
+
+      </main>
+    </div>
+
+
+
+
+
+
+
+
+
+      
+  </div>
+
+
+  )
+}
 export default App;
